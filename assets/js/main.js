6deae33d07e09a6c9c7f4c5a556cf4dd539ad641
aabereu <amando.abreu@trivago.com>
2016-12-21 22:55:31 +0100

    Added function to add date, draw table, basic sort

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,94 +4,87 @@
  3) On sort, clear tbody of all trs
  4) Loop through the tr array, create elements and add them to tbody
  */
-var manipulatable = function(element){
-    var _ = this;
-    _.classes = {
-        'sortable': 'sortable',
-        'down': 'sortable--down',
-        'up': 'sortable--up',
-        'th': 'thead th',
-        'tr': 'tbody tr'
-    };
-    _.element = element;
-    _.tables = document.querySelectorAll(_.element);
-    _.tableRows = [];
-    _.sortableKeys = []; /* Where we store values that we can use to sort the taable rows */
-    _.switchArrow = function(self, dir){
-        if(self.classList.contains(_.classes.up)){
-            self.classList.remove(_.classes.up);
-            self.classList.add(_.classes.down);
-        } else {
-            self.classList.remove(_.classes.down);
-            self.classList.add(_.classes.up);
-        }
-    };
 
-    _.sortByKey = function(key,desc) {
-        return function(a,b){
-            return desc ? ~~(a[key] < b[key]) : ~~(a[key] > b[key]);
-        }
-    };
+(function($) {
+    $.fn.manipulatable = function(params) {
+        return $(this).each(function() {
+            var self = $(this);
+            var tableRows = [];
+            var columnNames = [];
+            self.addData = function(data){
+                data.forEach(function(d){
+                    if(columnNames.length == 0){
+                        for(var key in d){
+                            columnNames.push(key);
+                        }
+                    }
+                    tableRows.push(d);
+                });
+                self.updateTable();
+            };
 
-    _.sort = function(){
-        var dir = 'up';
-        _.switchArrow(this, dir);
-        if(this.classList.contains(_.classes.up)){
-            _.tableRows = _.tableRows.sort(_.sortByKey(this.innerHTML, true));
-        } else {
-            _.tableRows = _.tableRows.sort(_.sortByKey(this.innerHTML));
-        }
-        for(var t = 0; t < _.tables.length; t++){
-            var thisTable = _.tables[t];
-            var thisTableTbody = thisTable.querySelector('tbody');
-            thisTableTbody.innerHTML = '';
-            for(var tr = 0; tr < _.tableRows.length; tr++){
-                var thisRow = _.tableRows[tr];
-                var rowElement = document.createElement('tr');
-                for(var key in thisRow){
-                    var text = document.createTextNode(thisRow[key]);
-                    var td = document.createElement('td');
-                    td.appendChild(text);
-                    rowElement.appendChild(td);
+            self.sortByKey = function(key,desc) {
+                return function(a,b){
+                    return desc ? ~~(a[key] < b[key]) : ~~(a[key] > b[key]);
                 }
-                thisTableTbody.appendChild(rowElement);
-            }
-        }
-    };
+            };
 
-    _.init = function(){
-        for(var i = 0; i < _.tables.length; i++){
-            var thisTable = _.tables[i];
+            self.sort = function(){
+                console.log('Sorting by: '+this.dataset.sort);
+                tableRows = tableRows.sort(self.sortByKey(this.innerHTML));
+                self.updateTable();
+            };
 
-            var ths = thisTable.querySelectorAll(_.classes.th);
-            for(var t = 0; t< ths.length; t++){
-                var thisTh = ths[t];
-                if(thisTh.classList.contains(_.classes.sortable)){
-                    thisTh.addEventListener('click', _.sort);
-                }
-                _.sortableKeys.push(thisTh.innerHTML);
-            }
+            self.updateTable = function(){
+                self.empty(); // Clear previous stuff
+                var thead   = document.createElement('thead');
+                var theadTr = document.createElement('tr');
+                columnNames.forEach(function(columnName){
+                    var text = document.createTextNode(columnName);
+                    var th   = document.createElement('th');
+                    th.appendChild(text);
+                    th.addEventListener('click', self.sort);
+                    th.dataset.sort = columnName;
+                    theadTr.appendChild(th);
 
-            var trs = thisTable.querySelectorAll(_.classes.tr);
-            for(var tr = 0; tr < trs.length; tr++){
-                var thisTr = trs[tr];
-                var thisTrTds = thisTr.querySelectorAll('td');
-                var tempTrArray = [];
-                for(var ttt = 0; ttt < thisTrTds.length; ttt++){
-                    tempTrArray[_.sortableKeys[ttt]] = thisTrTds[ttt].innerHTML;
-                }
-                _.tableRows.push(tempTrArray);
-            }
+                });
+                thead.appendChild(theadTr);
+                self.append(thead);
 
-            console.log(_.sortableKeys);
-            console.log(_.tableRows);
-        }
-    };
-    _.init();
-};
+                var tbody = document.createElement('tbody');
 
-var populate = function(){
-  $.ajax
-};
+                tableRows.forEach(function(row){
+                    var tbodyTr = document.createElement('tr');
+                    for(key in row){
+                        var text = document.createTextNode(row[key]);
+                        var td = document.createElement('td');
+                        td.appendChild(text);
+                        if(key == 'about'){
+                            td.classList.add('long-text');
+                        }
+                        tbodyTr.appendChild(td);
+                    }
+                    tbody.appendChild(tbodyTr);
 
-manipulatable('.manipulatable');
+
+                });
+                self.append(tbody);
+            };
+
+            self.init = function(){
+                $.ajax({
+                    url: 'data.json',
+                    dataType: 'json',
+                    success: function (data) {
+                        self.addData(data);
+                    }
+                });
+            };
+
+            self.init();
+
+        });
+    }
+})(jQuery);
+
+$('.manipulatable').manipulatable();
